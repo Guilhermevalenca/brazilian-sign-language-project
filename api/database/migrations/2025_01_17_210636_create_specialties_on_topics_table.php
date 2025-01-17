ad57e6b2d5db57c1111847e139c8304186b1cb95
Guilherme Valença <guilherm3vrp@gmail.com>
2025-01-17 18:12:12 -0300

    feat(migration): add pivot table for specialties and topics

---
+++ api/database/migrations/2025_01_17_210636_create_specialties_on_topics_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('specialties_on_topics', function (Blueprint $table) {
+            $table->foreignId('specialty_id')
+                ->references('id')
+                ->on('specialties');
+            $table->foreignId('topic_id')
+                ->references('id')
+                ->on('topics');
+            $table->primary(['specialty_id', 'topic_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('specialties_on_topics');
+    }
+};
