b473a570b15cfe33979877b147477fd33670bec5
Guilherme Valença <guilherm3vrp@gmail.com>
2025-01-24 14:05:16 -0300

    fix: add rule in UpdateKeywordRequest

--- api/app/Http/Requests/UpdateKeywordRequest.php
+++ api/app/Http/Requests/UpdateKeywordRequest.php
@@ -11,7 +11,7 @@ class UpdateKeywordRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class UpdateKeywordRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'unique:keywords,name'],
         ];
+
     }
 }
