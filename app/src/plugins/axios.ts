863129cc25b87c0f8329b12615e3472c69d66daa
Guilherme Valença <guilherm3vrp@gmail.com>
2025-03-12 16:34:18 -0300

    hotfix: nova logica de autenticação adicionada + verificação de email

--- app/src/plugins/axios.ts
+++ app/src/plugins/axios.ts
@@ -9,7 +9,7 @@ declare module '@vue/runtime-core' {
 export default defineNuxtPlugin((nuxtApp) => {
     const api: AxiosInstance = axios.create({
         baseURL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost',
-        withCredentials: true,
+        withCredentials: false,
         withXSRFToken: true,
     });
     nuxtApp.vueApp.config.globalProperties.$axios = api;
