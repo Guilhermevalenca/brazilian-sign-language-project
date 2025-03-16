1c3e481fdee02f4a9a5e0fe6203ad8d1d8b054ce
Guilherme Valença <guilherm3vrp@gmail.com>
2025-03-16 11:16:06 -0300

    fix: devolvendo o ssr para o projeto

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  ssr: false,
+  ssr: true,
   srcDir: 'src',
   css: ['~/assets/css/main.scss'],
   postcss: {
