15130b04c16a6adddb04adfe45311261797bada5
GuilhermeValenca <guilherm3vrp@gmail.com>
2025-07-06 00:03:57 -0300

    feat: adicionando ajuste na tabela courses

--- api/database/migrations/2025_07_06_030116_alter_field_in_courses_table.php
+++ api/database/migrations/2025_07_06_030116_alter_field_in_courses_table.php
@@ -4,17 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('courses', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('image');
-            $table->timestamps();
+        Schema::table('courses', function (Blueprint $table) {
+            $table->longText('image')->change();
         });
     }
 
@@ -23,6 +21,8 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('courses');
+        Schema::table('courses', function (Blueprint $table) {
+            $table->string('image')->change();
+        });
     }
 };
