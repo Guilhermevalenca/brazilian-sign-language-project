94519585cd8251f322fd47b01e64bb9f785af28e
Weydson Lino da Silva <wls10@discente.ifpe.edu.br>
2025-07-06 16:10:54 -0300

    refactor: corrigir prefixo da rota de monitoramento de usuários para 'user-monitoring' e remover rotas não utilizadas.

--- api/routes/userMonitoring.php
+++ api/routes/userMonitoring.php
@@ -3,20 +3,8 @@
 use App\Http\Controllers\UserMonitoringController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('userMonitoring')
+Route::prefix('user-monitoring')
     ->controller(UserMonitoringController::class)
     ->group(function () {
-
-        Route::get('/', 'index');
-        Route::get('/{subject}', 'show');
-
-        //Route::middleware(['auth:sanctum', 'ability:user-admin'])
-        //->group(function () {
-    
         Route::post('/', 'store');
-        Route::get('/{subject}/edit', 'edit');
-        Route::put('/{subject}', 'update');
-        Route::delete('/{subject}', 'destroy');
-
-        //});
     });
